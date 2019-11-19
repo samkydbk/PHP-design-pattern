11b227619a8af9dff8e33a43cd644f2526897f1f
sam <sam.zhang@orderplus.com>
2019-11-19 12:17:41 +0800

    提交代码

---
+++ 命名空间/Test11.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author        Yongtao Zhang <yyzm@vip.qq.com>
+ * @package       test1.php
+ * @createtime   2019-11-14 10:14
+ */
+namespace Test11;
+
+function test()
+{
+    echo __FILE__;
+}
---
+++ 类自动载入/Test21.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author        Yongtao Zhang <yyzm@vip.qq.com>
+ * @package       a.php
+ * @createtime   2019-11-06 18:05
+ */
+
+class Test21
+{
+    public static function test()
+    {
+        echo __FILE__;
+    }
+
+    public function test1()
+    {
+        echo 212121;
+    }
+}
